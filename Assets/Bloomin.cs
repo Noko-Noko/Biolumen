2e82f0b31e29b1392d2028d8bd087cc6228fb5f1
Max Krawiec <nokonoko@gmail.com>
2018-05-30 00:15:54 +0100

    audio, better controls, collisions

--- Assets/Bloomin.cs
+++ Assets/Bloomin.cs
@@ -6,61 +6,74 @@ using UnityEngine;
 public class Bloomin : MonoBehaviour {
 
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
-	// Use this for initialization
-	void Start () {
+    [SerializeField] float scaleModifier = 0.0025F;
+    [SerializeField] float upforce = 12f;
+    [SerializeField] float sideforce = 8f;
+
+    // Use this for initialization
+    void Start () {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        ProcessInputs();
+        up();
+        move();
+        deflate();
 	}
 
-    private void ProcessInputs()
+    private void up()
     {
-        float scale = 0.0025F;
-
         if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W))
         {
-            rigidBody.AddForce(Vector3.up);
-            transform.localScale += new Vector3(scale, scale, scale);
+            rigidBody.AddForce(Vector3.up * upforce);
+            transform.localScale += new Vector3(scaleModifier, scaleModifier, scaleModifier);
+            audioSource.Play();
         }
+    }
 
+    private void deflate()
+    {
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.LeftControl))
+        {
+            rigidBody.AddForce(Vector3.down * upforce);
+            rigidBody.AddForce(Vector3.left * sideforce);
+            if (transform.localScale.sqrMagnitude > 2 * 0.5)
+            {
+                transform.localScale -= new Vector3(scaleModifier * 5, scaleModifier * 5, scaleModifier * 5);
+            }
+            //audioSource.Play();
+        }
+    }
+
+    private void move()
+    {
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
-            rigidBody.AddForce(Vector3.left);
+            rigidBody.AddForce(Vector3.left * sideforce);
             if(transform.localScale.sqrMagnitude > 2 * 0.5)
             {
-                transform.localScale -= new Vector3(scale * 2, scale * 2, scale * 2);
+                transform.localScale -= new Vector3(scaleModifier, scaleModifier, scaleModifier);
             }
 
         } else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
-            print("right pressed");
-            rigidBody.AddForce(Vector3.right);
+            rigidBody.AddForce(Vector3.right * sideforce);
         }
+    }
 
-        //if (Input.GetKey(KeyCode.Space))
-        //{
-        //    switch (Input.inputString)
-        //    {
-        //        case "a":
-        //            Debug.Log("a pressed");
-        //            break;
-        //        case "d":
-        //            Debug.Log("d pressed");
-        //            break;
-        //        case "w":
-        //            Debug.Log("w pressed");
-        //            break;
-        //        case "Space":
-        //            Debug.Log("Space pressed");
-        //            break;
-        //        default:
-        //            Debug.Log("this is not a valid key");
-        //            break;
-        //    }
-        //}
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag) {
+            case "Friendly":
+                print("Freindly Collission detected");
+                break;
+            default:
+                print("Dead!");
+                break;
+        }
     }
 }
