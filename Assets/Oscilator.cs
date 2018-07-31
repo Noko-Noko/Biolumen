64097aeff57b29cd9059eb57b2224fb43a751963
Max Krawiec <nokonoko@gmail.com>
2018-07-31 23:51:29 +0100

    New assets, assets rearrange, oscilator

--- Assets/Oscilator.cs
+++ Assets/Oscilator.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 [DisallowMultipleComponent] public class Oscilator : MonoBehaviour {
 
-    [SerializeField] Vector3 movementFactor;
-    [Range(0, 1)] [SerializeField] float movementVector;
+    [SerializeField] Vector3 movementVector = new Vector3(2f, 2f, 2f);
+    [SerializeField] float speed = 2f;
 
     Vector3 startPosition;
 
@@ -16,7 +16,14 @@ using UnityEngine;
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position = startPosition + movementVector * movementFactor;
+        if (speed < 0f) return;
+        float cycles = Time.time / speed;
+        const float tau = Mathf.PI * 2f; // get a tau, 2 times Pi
+        float rawSinWave = Mathf.Sin(cycles * tau); // Return a loop of -1 to 1
+
+        float movementFactor = rawSinWave / 2f + .5f; // to get a variant from 0 to 1
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startPosition + offset;
 
     }
 }
